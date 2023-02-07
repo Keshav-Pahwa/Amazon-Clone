6d565ee37846fd3f82137cc15f805b8942919da8
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-02-08 00:29:25 +0530

    Top navbar completed
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,6 +29,23 @@ function Header() {
 					/>
 					<SearchIcon className="h-12 p-4" />
 				</div>
+				
+				{/* Right */}
+				<div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+					<div className="link ">
+						<p>Hello Keshav Pahwa</p>
+						<p className="font-extrabold md:text-sm">Account & lists</p>
+					</div>
+					<div className="link">
+						<p>Returns</p>
+						<p className="font-extrabold md:text-sm">& Orders</p>
+					</div>
+					<div className="relative link flex container items-center">
+						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+						<ShoppingCartIcon className="h-10"/>
+						<p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+					</div>
+				</div>
 			</div>
 			{/* Bottom Nav */}
 			<div></div>
