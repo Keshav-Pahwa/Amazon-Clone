5648198a6b9c954be606ca66ef4f940dddb28ef9
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-02-09 00:29:38 +0530

    Added Banner and carousel
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,7 +48,7 @@ function Header() {
 				</div>
 			</div>
 			{/* Bottom Nav */}
-			<div className='flex items-center space-x-4 p-2 pl-4 bg-amazon_blue-light text-white text-sm'>
+			<div className='flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm'>
 				<p className='link flex items-center'>
 					<MenuIcon className='h-6 mr-1' />
 					All
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
 	return (
@@ -7,9 +8,18 @@ export default function Home() {
 			<Head>
 				<title>Amazon 2.0</title>
 			</Head>
+			
 			{/* Header */}
-      
 			<Header />
+
+			<main className='max-w-screen-2xl mx-auto'>
+
+			{/* Banner */}
+			<Banner />
+
+			</main>
+      
+			
 		</div>
 	);
 }
