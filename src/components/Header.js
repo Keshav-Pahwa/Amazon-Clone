23a8e566017753bdbeae220263e943214599b0ac
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-02-09 00:01:19 +0530

    Header component finished Bottom Nav bar added
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,7 +48,21 @@ function Header() {
 				</div>
 			</div>
 			{/* Bottom Nav */}
-			<div></div>
+			<div className='flex items-center space-x-4 p-2 pl-4 bg-amazon_blue-light text-white text-sm'>
+				<p className='link flex items-center'>
+					<MenuIcon className='h-6 mr-1' />
+					All
+				</p>
+				<p className='link'>Prime Video</p>
+				<p className='link'>Amazon Business</p>
+				<p className='link'>Today's Deals</p>
+				<p className='link hidden lg:inline-flex'>Electronics</p>
+				<p className='link hidden lg:inline-flex'>Food & Grocery</p>
+				<p className='link hidden lg:inline-flex'>Prime</p>
+				<p className='link hidden lg:inline-flex'>Buy Again</p>
+				<p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+				<p className='link hidden lg:inline-flex'>Health & Personal Care</p>
+				</div>
 		</header>
 	);
 }
