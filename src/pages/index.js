1f872a069710666c2782296576c8f02e76ee2448
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-02-10 00:40:23 +0530

    Product component added into the build
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
 	return (
 		<div>
 			<Head>
@@ -17,9 +18,26 @@ export default function Home() {
 			{/* Banner */}
 			<Banner />
 
+			{/* Product Feed */}
+			<ProductFeed products = {products}/>
 			</main>
       
 			
 		</div>
 	);
 }
+
+export async function  getServerSideProps(context) {
+	const products = await fetch("https://fakestoreapi.com/products").then(
+		(res) => res.json()
+	);
+
+	return {
+		props: {
+			products,
+		},
+	}
+}
+
+
+// https://fakestoreapi.com/products
