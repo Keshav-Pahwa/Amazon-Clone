6ec1899225cf447b0c9b4f08286a3468f9bfad0f
Keshav Pahwa <keshavpahwa01@gmail.com>
2023-04-04 19:51:20 +0530

    minor update-2
    Signed-off-by: Keshav Pahwa <keshavpahwa01@gmail.com>

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,8 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-	providers: [ GoogleProvider({
-		clientId: process.env.GOOGLE_ID,
-		clientSecret: process.env.GOOGLE_SECRET,
-	}) ]
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+		}),
+	],
+	secret: process.env.NEXT_PUBLIC_SECRET,
 });
